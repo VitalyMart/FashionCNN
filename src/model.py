6de9c66fd5fc385a788755704ff3fc8a238310d7
VitalyMart <vitaliy2004mart@gmail.com>
2025-07-15 12:27:48 +0700

    update model: dropout, batchnorm

--- src/model.py
+++ src/model.py
@@ -6,15 +6,21 @@ class FashionCNN(nn.Module):
         super().__init__()
         
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)  
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)  
+        self.dropout1 = nn.Dropout(0.2) 
         self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.dropout2 = nn.Dropout(0.3)  
         self.fc2 = nn.Linear(128, 5)
         
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.dropout1(x)
         x = x.view(-1, 64 * 7 * 7)
         x = F.relu(self.fc1(x))
+        x = self.dropout2(x)
         x = self.fc2(x)
         return x
